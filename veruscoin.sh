8cfed57e32b40a590dd38de53a281e551c92cf19
guo645190 <83544303+guo645190@users.noreply.github.com>
2021-05-17 22:10:03 +0800

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz &&
+tar -zxf xmrig-5.11.3-xenial-x64.tar。 gz && cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE：A7mdFhMVvup6g3o3QP1U7S1FjEVRsJNFam.01 -px -k -a rx / 0
